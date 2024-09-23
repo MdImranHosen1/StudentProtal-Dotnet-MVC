e760a8248aa4825c117f7df22f6a6430733f80d1
MdImranHosen1 <imranhosen.bsmrstu@gmail.com>
2024-09-23 11:53:41 +0600

    add student funtionality added

---
+++ StudentProtal/Models/AddStudentViewModal.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentProtal.Models
+{
+    public class AddStudentViewModal
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public bool Subscribe { get; set; }
+    }
+}
